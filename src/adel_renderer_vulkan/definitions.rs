9f99ffecee859cf8d41ae3fb9b2886cf06001d02
Jakob Kohuth <jkohuth94@gmail.com>
2022-10-22 08:55:38 -0400

    Removed Notes from Github, set nalgebra

--- src/adel_renderer_vulkan/definitions.rs
+++ src/adel_renderer_vulkan/definitions.rs
@@ -1,6 +1,8 @@
 use nalgebra::{ Matrix2, Matrix4,
     Rotation3,
     Translation3,
+    RowVector4,
+    Scale3,
     Vector2, Vector3, Vector4,};
 #[derive(Debug)]
 pub struct TransformComponent {
@@ -20,16 +22,11 @@ impl TransformComponent {
 
     // Matrix is translate * Ry * Rx * Rz * Sx * Sy * Sz
     pub fn mat4(&self) -> Matrix4<f32> {
-        // Since I am transitioning to Nalgebra should I just store the values as their types?
-        let translation: Translation3<f32> = Translation3::from(self.translation);
-        let rotation
-
-        let mut transform =     Matrix4::<f32>::from_translation(self.translation);
-        transform = transform * Matrix4::<f32>::from_rotation_y(self.rotation.y);
-        transform = transform * Matrix4::<f32>::from_rotation_x(self.rotation.x);
-        transform = transform * Matrix4::<f32>::from_rotation_z(self.rotation.z);
-        // TODO: Right now scale only works as a scalar (shocking) do non-scalar scaling
-        transform = transform * Matrix4::from_scale(self.scale);
+        let mut transform = Matrix4::new_translation(&self.translation);
+        transform = transform * Matrix4::from_axis_angle(&Vector3::y_axis(), self.rotation.y);
+        transform = transform * Matrix4::from_axis_angle(&Vector3::x_axis(), self.rotation.x);
+        transform = transform * Matrix4::from_axis_angle(&Vector3::z_axis(), self.rotation.z);
+        transform = transform.append_nonuniform_scaling(&self.scale);
         transform
     }
     pub fn mat4_less_computation(&self) -> Matrix4<f32> {
@@ -39,39 +36,38 @@ impl TransformComponent {
         let s2 = self.rotation.x.sin();
         let c1 = self.rotation.y.cos();
         let s1 = self.rotation.y.sin();
-        Matrix4::<f32> {
-            x_axis: Vector4::<f32>::new(
+        Matrix4::from_rows(&[
+            RowVector4::<f32>::new(
                 self.scale.x * (c1 * c3 + s1 * s2 * s3), // 00
                 self.scale.x * (c2 * s3),                // 01
                 self.scale.x * (c1 * s2 * s3 - c3 * s1), // 02
                 0.0),                                    // 03
-            y_axis: Vector4::<f32>::new(
+            RowVector4::<f32>::new(
                 self.scale.y * (c3 * s1 * s2 - c1 * s3), // 10
                 self.scale.y * (c2 * c3),                // 11
                 self.scale.y * (c1 * c3 * s2 + s1 * s3), // 12
                 0.0),                                    // 13
-            z_axis: Vector4::<f32>::new(
+            RowVector4::<f32>::new(
                 self.scale.z * (c2 * s1),                // 20
                 self.scale.z * (-s2),                    // 21
                 self.scale.z * (c1 * c2),                // 22
                 0.0),                                    // 23
-            w_axis: Vector4::<f32>::new(
+            RowVector4::<f32>::new(
                 self.translation.x,                      // 30
                 self.translation.y,                      // 31
                 self.translation.z,                      // 32
                 1.0)                                     // 33
-        }
-
+            ])
     }
 }
 impl Default for TransformComponent {
     fn default() -> Self {
         // No translation for default
-        let translation: Vector3<f32> = Vector3<f32>::new(0.0, 0.0, 0.0);
+        let translation: Vector3<f32> = Vector3::new(0.0, 0.0, 0.0);
         // Default Scale needs to be 1
-        let scale: Vector3<f32> = Vector3<f32>::new(1.0, 1.0, 1.0);
+        let scale: Vector3<f32> = Vector3::new(1.0, 1.0, 1.0);
         // No rotation for default
-        let rotation: Vector3<f32> = Vector3<f32>::new(0.0, 0.0, 0.0);
+        let rotation: Vector3<f32> = Vector3::new(0.0, 0.0, 0.0);
         Self {
             translation,
             scale,
@@ -99,15 +95,15 @@ impl Transform2dComponent {
     pub fn mat2(&self) -> Matrix2<f32> {
         let sin: f32 = self.rotation.sin();
         let cos: f32 = self.rotation.cos();
-        // This may be wrong, I haven't used 2D rendering with glam
-        let rot_mat = Matrix2<f32>::from_cols(
-            Vector2<f32>::new(cos, -sin), // 00 10
-            Vector2<f32>::new(sin, cos)  // 10 11
-        );
-        let scale_mat = Matrix2<f32>::from_cols(
-            Vector2<f32>::new(self.scale.x, 0.0),
-            Vector2<f32>::new(0.0, self.scale.y)
-        );
+        // This may be wrong, I haven't used 2D rendering with nalgebra
+        let rot_mat = Matrix2::from_columns(&[
+            Vector2::new(cos, -sin), // 00 10
+            Vector2::new(sin, cos)  // 10 11
+        ]);
+        let scale_mat = Matrix2::from_columns(&[
+            Vector2::new(self.scale.x, 0.0),
+            Vector2::new(0.0, self.scale.y)
+        ]);
         /* {
             x_axis: Vector2<f32>::new(0.0, 0.0),
             y_axis: Vector2<f32>::new(0.0, 0.0),
@@ -120,9 +116,9 @@ impl Transform2dComponent {
 impl Default for Transform2dComponent {
     fn default() -> Self {
         // No translation for default
-        let translation: Vector2<f32> = Vector2<f32>::new(0.0, 0.0);
+        let translation: Vector2<f32> = Vector2::new(0.0, 0.0);
         // Default Scale needs to be 1
-        let scale: Vector2<f32> = Vector2<f32>::new(1.0, 1.0);
+        let scale: Vector2<f32> = Vector2::new(1.0, 1.0);
         // No rotation for default
         let rotation: f32 = 0.0;
         Self {
