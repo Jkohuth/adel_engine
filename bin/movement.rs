4e160fb7d85a34fbde0eb9cd078e6b1dcb1c1cda
Jakob Kohuth <jkohuth94@gmail.com>
2022-12-28 11:01:08 -0600

    Collision Skeleton implementation

--- bin/movement.rs
+++ bin/movement.rs
@@ -10,39 +10,42 @@ fn main() {
     let mut world = World::new();
 
     let vertices: Vec<Vertex> = vec![
-        Vertex { position: Vector3::new(-0.5, -0.5, 0.0), color: Vector3::new(1.0, 0.0, 0.0) },
-        Vertex { position: Vector3::new(0.5, -0.5, 0.0), color: Vector3::new(0.0, 0.0, 1.0) },
-        Vertex { position: Vector3::new(0.5, 0.5, 0.0), color: Vector3::new(0.0, 1.0, 0.0) },
-        Vertex { position: Vector3::new(-0.5, 0.5, 0.0), color: Vector3::new(1.0, 0.0, 0.0) },
+        Vertex { position: Vector3::new(-0.25, -0.25, 0.0), color: Vector3::new(0.0, 0.0, 1.0) },
+        Vertex { position: Vector3::new( 0.25, -0.25, 0.0), color: Vector3::new(0.0, 0.0, 1.0) },
+        Vertex { position: Vector3::new( 0.25,  0.25, 0.0), color: Vector3::new(0.0, 0.0, 1.0) },
+        Vertex { position: Vector3::new(-0.25,  0.25, 0.0), color: Vector3::new(0.0, 0.0, 1.0) },
     ];
     let indices: Vec<u16> = vec![0, 1, 2, 2, 3, 0];
-
     let vertices2: Vec<Vertex> = vec![
-        Vertex { position: Vector3::new(-0.5, -0.5, 0.0), color: Vector3::new(0.0, 0.0, 1.0)},
-        Vertex { position: Vector3::new(0.5, -0.5, 0.0), color: Vector3::new(0.0, 0.0, 1.0)},
-        Vertex { position: Vector3::new(0.5, 0.5, 0.0), color: Vector3::new(0.0, 0.0, 1.0)},
+        Vertex { position: Vector3::new(-0.25, -0.25, 0.0), color: Vector3::new(0.0, 1.0, 0.0) },
+        Vertex { position: Vector3::new( 0.25, -0.25, 0.0), color: Vector3::new(0.0, 1.0, 0.0) },
+        Vertex { position: Vector3::new( 0.25,  0.25, 0.0), color: Vector3::new(0.0, 1.0, 0.0) },
+        Vertex { position: Vector3::new(-0.25,  0.25, 0.0), color: Vector3::new(0.0, 1.0, 0.0) },
     ];
-    let indices2: Vec<u16> = vec![0, 1, 2];
-    let vi_component2 = VertexIndexComponent {
-        vertices: vertices2,
-        indices: indices2
-    };
+
     let vi_component = VertexIndexComponent {
         vertices,
-        indices
+        indices: indices.clone()
     };
-    let transform = TransformComponent::default();
+    let vi_component2 = VertexIndexComponent {
+        vertices: vertices2,
+        indices: indices
+    };
+    let mut transform = TransformComponent::default();
+    transform.translation.x -= 0.5;
+    let mut transform2 = TransformComponent::default();
+    transform2.translation.x += 0.5;
     let camera_transform = TransformComponent::default();
 
-    log::info!("Transform default {:?}", &transform);
-    log::info!("Nalgebra matrix order identity \n{:?}", Matrix4::<f32>::identity());
     let keyboard = KeyboardComponent {};
     let entity = world.new_entity();
     world.add_component_to_entity(entity, vi_component);
     world.add_component_to_entity(entity, transform);
-    let camera_entity = world.new_entity();
     world.add_component_to_entity(entity, keyboard);
-    //world.add_component_to_entity(camera_entity, keyboard);
+    let entity2 = world.new_entity();
+    world.add_component_to_entity(entity2, vi_component2);
+    world.add_component_to_entity(entity2, transform2);
+    let camera_entity = world.new_entity();
     world.add_component_to_entity(camera_entity, camera_transform);
     let app = Application::new(world);
     app.main_loop();
---
+++ src/adel_physics/collision.rs
@@ -0,0 +1,79 @@
+
+use crate::adel_ecs::{System, World};
+use nalgebra::{Vector2, Vector3};
+// BoxCollider2D Component
+// Requires a location (offset from the parent/entity?)
+// Requires extents
+enum Direction {
+    UP = 0,
+    DOWN = 1,
+    RIGHT = 2,
+    LEFT = 3,
+}
+impl TryFrom<isize> for Direction {
+    type Error = ();
+    fn try_from(value: isize) -> Result<Direction, Self::Error> {
+        match value {
+            dir if dir == Direction::UP as isize => Ok(Direction::UP),
+            dir if dir == Direction::DOWN as isize => Ok(Direction::DOWN),
+            dir if dir == Direction::RIGHT as isize => Ok(Direction::RIGHT),
+            dir if dir == Direction::LEFT as isize => Ok(Direction::LEFT),
+            _ => Err(())
+        }
+    }
+}
+impl Direction {
+    pub fn check_direction(direction: &Vector2::<f32>) -> Direction {
+        let compass: Vec<Vector2<f32>> = vec![
+            Vector2::new( 0.0, -1.0), // UP
+            Vector2::new( 0.0,  1.0), // DOWN
+            Vector2::new( 1.0,  0.0), // RIGHT
+            Vector2::new(-1.0,  0.0), // LEFT
+        ];
+        let mut max: f32 = 0.0;
+        let mut best_match: isize = -1;
+        for i in compass.iter().enumerate() {
+            let dot_product = Vector2::dot(&direction.normalize(), direction);
+            if dot_product > max {
+                max = dot_product;
+                best_match = i.0 as isize;
+            }
+        }
+        Direction::try_from(best_match).unwrap()
+    }
+}
+pub trait Collider {
+    fn center(&self) -> Vector3::<f32>;
+    fn check_direction(&self, dir: Vector2::<f32>) {
+
+    }
+    fn check_box_collision(&self, other: &BoxCollider2D) -> bool;
+    fn name(&self) -> &'static str {
+        "collider"
+    }
+}
+pub struct BoxCollider2D {
+    center: Vector3::<f32>,
+    extents: Vector2::<f32>
+}
+impl Collider for BoxCollider2D {
+    fn center(&self) -> Vector3::<f32> {
+        self.center
+    }
+    fn check_box_collision(&self, other: &BoxCollider2D) -> bool {
+        false
+    }
+}
+impl System for dyn Collider {
+    fn startup(&mut self, world: &mut World) {}
+    fn run(&mut self, world: &mut World) {}
+    fn shutdown(&mut self, world: &mut World) {}
+    fn name(&self) -> &'static str {
+        self.name()
+    }
+
+}
+// ColliderSystem2D
+// Startup ()
+// Run() -> Compare against
+
